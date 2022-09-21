d1723b9ef476e5d185827bc0a7f5e9a76e4ebe93
lamnaourm <mohammed.lamnaour@gmail.com>
2022-09-21 16:50:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+var etudiants = [
+  { nom: "Manar", math: 10.5, phys: 12.0, fr: 16.0 },
+  { nom: "Mohamed", math: 13.0, phys: 18.0, fr: 7.0 },
+  { nom: "Khalid", math: 15.0, phys: 15.0, fr: 13.0 },
+  { nom: "Ikbal", math: 2.6, phys: 9.0, fr: 10.0 },
+  { nom: "Aziz", math: 10.0, phys: 11.5, fr: 15.0 },
+  { nom: "Said", math: 12.0, phys: 5.0, fr: 13.0 },
+  { nom: "Moujahid", math: 20.0, phys: 15.0, fr: 18.0 },
+  { nom: "Riyad", math: 18.0, phys: 13.0, fr: 15.0 },
+];
+
+
+class Liste1 extends React.Component{
+  render(){
+    return <ul>
+      {etudiants.map(item => <li>{item.nom} - {((item.math+item.phys+item.fr)/3).toFixed(2)}</li>)}
+    </ul>
+  }
+}
+
+class Liste2 extends React.Component {
+  render(){
+    return <table>
+      <tr>
+        <th>Nom</th>
+        <th>Math</th>
+        <th>Phys</th>
+        <th>Fr</th>
+        <th>Moyenne</th>
+      </tr>
+      {etudiants.map(item => 
+      <tr>
+        <td>{item.nom}</td>
+        <td>{item.math}</td>
+        <td>{item.phys}</td>
+        <td>{item.fr}</td>
+        <td>{((item.math+item.phys+item.fr)/3).toFixed(2)}</td>
+      </tr>
+      )}
+    </table>
+  }
+}
+function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Liste1/>
+    <Liste2 />
     </div>
   );
 }
